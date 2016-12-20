72a1bfe5c9c54c7cb2c4d51cfc297786de1291a4
akito hataguchi <hataguchi@motionpicture.jp>
2016-12-20 16:32:10 +0900

    SASAKI_TICKET-11 redisアカウント修正

--- apps/frontend/middlewares/session.js
+++ apps/frontend/middlewares/session.js
@@ -1,16 +1,26 @@
 "use strict";
 const session = require('express-session');
-//import redis = require('redis');
+const config = require('config');
+const connectRedis = require('connect-redis');
+// import redis = require('redis');
 const redis = require('redis');
+let redisClient = redis.createClient(config.get('redis_port'), config.get('redis_host'), {
+    password: config.get('redis_key'),
+    tls: {
+        servername: config.get('redis_host')
+    },
+    return_buffers: true
+});
+let RedisStore = connectRedis(session);
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = session({
     secret: 'FrontendSecret',
     resave: false,
     rolling: true,
     saveUninitialized: false,
-    // store: new RedisStore({
-    //     client: redisClient
-    // }),
+    store: new RedisStore({
+        client: redisClient
+    }),
     cookie: {
         // secure: true,
         httpOnly: true,
--- apps/frontend/middlewares/session.ts
+++ apps/frontend/middlewares/session.ts
@@ -1,31 +1,31 @@
 import session = require('express-session');
 import config = require('config');
 import connectRedis = require('connect-redis');
-//import redis = require('redis');
+// import redis = require('redis');
 const redis = require('redis');
 
-// let redisClient = redis.createClient(
-//     config.get<number>('redis_port'),
-//     config.get<string>('redis_host'),
-//     {
-//         password: config.get<string>('redis_key'),
-//         tls: {
-//             servername: config.get<string>('redis_host')
-//         },
-//         return_buffers: true
-//     }
-// );
+let redisClient = redis.createClient(
+    config.get<number>('redis_port'),
+    config.get<string>('redis_host'),
+    {
+        password: config.get<string>('redis_key'),
+        tls: {
+            servername: config.get<string>('redis_host')
+        },
+        return_buffers: true
+    }
+);
 
-// let RedisStore = connectRedis(session);
+let RedisStore = connectRedis(session);
 
 export default session({
     secret: 'FrontendSecret', 
     resave: false,
     rolling: true,
     saveUninitialized: false,
-    // store: new RedisStore({
-    //     client: redisClient
-    // }),
+    store: new RedisStore({
+        client: redisClient
+    }),
     cookie: {
         // secure: true,
         httpOnly: true,
