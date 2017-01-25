c7450d2819fbd8d0409a74d95b7c57c101f7fc09
akito hataguchi <hataguchi@motionpicture.jp>
2017-01-25 14:50:07 +0900

    SASAKI_TICKET-36 自動テストコード作成

--- test/test.js
+++ test/test.js
@@ -9,8 +9,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const webdriver = require("selenium-webdriver");
 let capabilities = {
-    'browserstack.user': 'akitohataguchi1',
-    'browserstack.key': '91NcdgSQ6KR9vqHP33xp',
+    'browserstack.user': 'tetsuyamazaki1',
+    'browserstack.key': 'Ef2optk5kygevGh5muCg',
     'browserName': 'Chrome',
     'browser_version': '55.0',
     'os': 'Windows',
@@ -18,10 +18,17 @@ let capabilities = {
     'resolution': '1920x1080',
     'browserstack.debug': true
 };
+console.log(`
+ブラウザ: ${capabilities.browserName} ${capabilities.browser_version}
+OS: ${capabilities.os} ${capabilities.os_version}
+画面: ${capabilities.resolution}
+`);
 let driver = new webdriver.Builder()
     .usingServer('http://hub-cloud.browserstack.com/wd/hub')
     .withCapabilities(capabilities)
     .build();
+console.log('要素表示待機20秒');
+driver.manage().timeouts().implicitlyWait(20000);
 driver.get('https://devsasakiticketfrontendprototypewebapp-sasaki-ticket-11.azurewebsites.net/');
 let by = webdriver.By;
 main().then(() => {
@@ -41,7 +48,7 @@ function main() {
 }
 function performanceSelect() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield driver.sleep(1000);
+        yield driver.findElement(by.className('performances'));
         console.log('パフォーマンス一覧');
         let lists = yield driver
             .findElement(by.className('performances'))
@@ -58,72 +65,95 @@ function performanceSelect() {
 }
 function seat() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield driver.sleep(1000);
+        yield driver.findElement(by.className('purchase-seat'));
         console.log('座席選択');
-        yield driver.sleep(3000);
-        let seats = yield driver
+        let defaultSeats = yield driver
             .findElement(by.className('screen'))
             .findElements(by.className('default'));
-        let num = Math.floor(Math.random() * seats.length - 1);
-        console.log('座席選択' + num);
-        yield seats[num].click();
-        yield nextClick();
+        let count = (defaultSeats.length > 5) ? Math.floor(Math.random() * (5 - 1) + 1) : Math.floor(Math.random() * (defaultSeats.length - 1) + 1);
+        console.log(`座席選択数: ${count}`);
+        for (let i = 0; i < count; i++) {
+            let seats = yield driver
+                .findElement(by.className('screen'))
+                .findElements(by.className('default'));
+            let num = Math.floor(Math.random() * seats.length - 1);
+            console.log(`座席: ${num}`);
+            yield seats[num].click();
+        }
+        yield driver
+            .findElement(by.className('button-area'))
+            .findElement(by.className('next-button'))
+            .findElement(by.tagName('button'))
+            .click();
+        console.log('次へクリック');
     });
 }
 function ticket() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield driver.sleep(3000);
+        yield driver.findElement(by.className('purchase-ticket'));
         console.log('券種選択');
         let seats = yield driver
             .findElement(by.className('seats'))
             .findElements(by.tagName('li'));
-        console.log('券種選択' + seats.length + '個');
+        console.log(`座席数: ${seats.length}`);
         for (var i = 0; i < seats.length; i++) {
             yield seats[i].findElement(by.tagName('a')).click();
             let tickets = yield driver
                 .findElement(by.className('modal'))
                 .findElements(by.className('blue-button'));
             let num = Math.floor(Math.random() * tickets.length - 1);
-            console.log('券種選択' + num);
+            console.log(`券種: ${num}`);
             yield tickets[num]
                 .findElement(by.tagName('a'))
                 .click();
         }
-        yield nextClick();
+        yield driver
+            .findElement(by.className('button-area'))
+            .findElement(by.className('next-button'))
+            .findElement(by.tagName('button'))
+            .click();
+        console.log('次へクリック');
     });
 }
 function input() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield driver.sleep(1000);
+        yield driver.findElement(by.className('purchase-input'));
         console.log('購入者情報入力');
         yield driver.findElement(by.name('last_name_hira')).clear();
         yield driver.findElement(by.name('first_name_hira')).clear();
-        yield driver.findElement(by.name('mail')).clear();
+        yield driver.findElement(by.name('mail_addr')).clear();
         yield driver.findElement(by.name('mail_confirm')).clear();
-        yield driver.findElement(by.name('tel')).clear();
+        yield driver.findElement(by.name('tel_num')).clear();
+        console.log('入力削除');
         yield driver.findElement(by.name('last_name_hira')).sendKeys('もーしょん');
         yield driver.findElement(by.name('first_name_hira')).sendKeys('ぴくちゃー');
-        yield driver.findElement(by.name('mail')).sendKeys('hataguchi@motionpicture.jp');
+        yield driver.findElement(by.name('mail_addr')).sendKeys('hataguchi@motionpicture.jp');
         yield driver.findElement(by.name('mail_confirm')).sendKeys('hataguchi@motionpicture.jp');
-        yield driver.findElement(by.name('tel')).sendKeys('0362778824');
-        yield driver.findElement(by.name('agree')).click();
+        yield driver.findElement(by.name('tel_num')).sendKeys('0362778824');
+        yield driver.findElement(by.className('agree-label')).click();
         yield driver.findElement(by.name('cardno')).sendKeys('4111111111111111');
         yield driver.findElement(by.name('credit_year')).sendKeys('2017');
         yield driver.findElement(by.name('credit_month')).sendKeys('10');
         yield driver.findElement(by.name('securitycode')).sendKeys('123');
-        yield nextClick();
+        console.log('入力完了');
+        yield driver
+            .findElement(by.className('button-area'))
+            .findElement(by.className('next-button'))
+            .findElement(by.tagName('button'))
+            .click();
+        console.log('次へクリック');
     });
 }
 function confirm() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield driver.sleep(1000);
+        yield driver.findElement(by.className('purchase-confirm'));
         console.log('購入者内容確認');
-        yield nextClick();
+        yield driver
+            .findElement(by.className('purchase-confirm'))
+            .findElement(by.className('button-area'))
+            .findElement(by.className('next-button'))
+            .findElement(by.tagName('button'))
+            .click();
+        console.log('次へクリック');
     });
-}
-function nextClick() {
-    driver.findElement(by.className('button-area'))
-        .findElement(by.className('next-button'))
-        .findElement(by.tagName('button'))
-        .click();
 }
--- test/test.ts
+++ test/test.ts
@@ -2,8 +2,10 @@ import webdriver = require('selenium-webdriver');
 
 // Input capabilities
 let capabilities = {
-    'browserstack.user': 'akitohataguchi1',
-    'browserstack.key': '91NcdgSQ6KR9vqHP33xp',
+    // 'browserstack.user': 'akitohataguchi1',
+    // 'browserstack.key': '91NcdgSQ6KR9vqHP33xp',
+    'browserstack.user': 'tetsuyamazaki1',
+    'browserstack.key': 'Ef2optk5kygevGh5muCg',
     'browserName': 'Chrome',
     'browser_version': '55.0',
     'os': 'Windows',
@@ -11,11 +13,21 @@ let capabilities = {
     'resolution': '1920x1080',
     'browserstack.debug': true
 }
+//設定
+console.log(`
+ブラウザ: ${capabilities.browserName} ${capabilities.browser_version}
+OS: ${capabilities.os} ${capabilities.os_version}
+画面: ${capabilities.resolution}
+`);
 
 let driver = new webdriver.Builder()
     .usingServer('http://hub-cloud.browserstack.com/wd/hub')
     .withCapabilities(capabilities)
     .build();
+
+//要素表示待機20秒
+console.log('要素表示待機20秒')
+driver.manage().timeouts().implicitlyWait(20000);
 
 //アクセス
 driver.get('https://devsasakiticketfrontendprototypewebapp-sasaki-ticket-11.azurewebsites.net/');
@@ -41,8 +53,8 @@ async function main() {
 }
 
 //パフォーマンス一覧
-async function performanceSelect() {
-    await driver.sleep(1000);
+async function performanceSelect(): Promise<void> {
+    await driver.findElement(by.className('performances'));
     console.log('パフォーマンス一覧');
     let lists = await driver
         .findElement(by.className('performances'))
@@ -58,77 +70,103 @@ async function performanceSelect() {
 }
 
 //座席選択
-async function seat() {
-    await driver.sleep(1000);
+async function seat(): Promise<void> {
+    await driver.findElement(by.className('purchase-seat'));
     console.log('座席選択');
-    await driver.sleep(3000);
-    let seats = await driver
+    let defaultSeats = await driver
         .findElement(by.className('screen'))
         .findElements(by.className('default'));
-    let num = Math.floor(Math.random() * seats.length - 1);
-    console.log('座席選択' + num);
-    await seats[num].click();
-    await nextClick();
+    let count = (defaultSeats.length > 5) ? Math.floor(Math.random() * (5 - 1) + 1) : Math.floor(Math.random() * (defaultSeats.length - 1) + 1);
+    console.log(`座席選択数: ${count}`);
+    for (let i = 0; i < count; i++) {
+        let seats = await driver
+            .findElement(by.className('screen'))
+            .findElements(by.className('default'));
+        let num = Math.floor(Math.random() * seats.length - 1);
+        console.log(`座席: ${num}`);
+        await seats[num].click();
+    }    
+    
+    await driver
+        .findElement(by.className('button-area'))
+        .findElement(by.className('next-button'))
+        .findElement(by.tagName('button'))
+        .click();
+    console.log('次へクリック');
 }
 
 //券種選択
-async function ticket() {
-    await driver.sleep(3000);
+async function ticket(): Promise<void> {
+    await driver.findElement(by.className('purchase-ticket'));
     console.log('券種選択');
     let seats = await driver
         .findElement(by.className('seats'))
         .findElements(by.tagName('li'));
-    console.log('券種選択' + seats.length + '個');
+    console.log(`座席数: ${seats.length}`);
     for (var i = 0; i < seats.length; i++) {
         await seats[i].findElement(by.tagName('a')).click();
+        // await driver.sleep(1000);
         let tickets = await driver
             .findElement(by.className('modal'))
             .findElements(by.className('blue-button'));
         let num = Math.floor(Math.random() * tickets.length - 1);
-        console.log('券種選択' + num);
+        console.log(`券種: ${num}`);
         await tickets[num]
             .findElement(by.tagName('a'))
             .click();
+        // await driver.sleep(1000);
     }
-    
-    await nextClick();
+
+    await driver
+        .findElement(by.className('button-area'))
+        .findElement(by.className('next-button'))
+        .findElement(by.tagName('button'))
+        .click();
+    console.log('次へクリック');
 }
 
 //購入者情報入力
-async function input() {
-    await driver.sleep(1000);
+async function input(): Promise<void> {
+    await driver.findElement(by.className('purchase-input'));
     console.log('購入者情報入力');
     await driver.findElement(by.name('last_name_hira')).clear();
     await driver.findElement(by.name('first_name_hira')).clear();
-    await driver.findElement(by.name('mail')).clear();
+    await driver.findElement(by.name('mail_addr')).clear();
     await driver.findElement(by.name('mail_confirm')).clear();
-    await driver.findElement(by.name('tel')).clear();
-
+    await driver.findElement(by.name('tel_num')).clear();
+    console.log('入力削除');
     await driver.findElement(by.name('last_name_hira')).sendKeys('もーしょん');
     await driver.findElement(by.name('first_name_hira')).sendKeys('ぴくちゃー');
-    await driver.findElement(by.name('mail')).sendKeys('hataguchi@motionpicture.jp');
+    await driver.findElement(by.name('mail_addr')).sendKeys('hataguchi@motionpicture.jp');
     await driver.findElement(by.name('mail_confirm')).sendKeys('hataguchi@motionpicture.jp');
-    await driver.findElement(by.name('tel')).sendKeys('0362778824');
-    await driver.findElement(by.name('agree')).click();
+    await driver.findElement(by.name('tel_num')).sendKeys('0362778824');
+    await driver.findElement(by.className('agree-label')).click();
     await driver.findElement(by.name('cardno')).sendKeys('4111111111111111');
     await driver.findElement(by.name('credit_year')).sendKeys('2017');
     await driver.findElement(by.name('credit_month')).sendKeys('10');
     await driver.findElement(by.name('securitycode')).sendKeys('123');
-    await nextClick();
-}
-
-//購入者内容確認
-async function confirm() {
-    await driver.sleep(1000);
-    console.log('購入者内容確認');
-    await nextClick();
-}
-
-function nextClick() {
-    driver.findElement(by.className('button-area'))
+    console.log('入力完了');
+    await driver
+        .findElement(by.className('button-area'))
         .findElement(by.className('next-button'))
         .findElement(by.tagName('button'))
         .click();
+    console.log('次へクリック');
 }
+
+//購入者内容確認
+async function confirm(): Promise<void> {
+    await driver.findElement(by.className('purchase-confirm'));
+    console.log('購入者内容確認');
+    await driver
+        .findElement(by.className('purchase-confirm'))
+        .findElement(by.className('button-area'))
+        .findElement(by.className('next-button'))
+        .findElement(by.tagName('button'))
+        .click();
+    console.log('次へクリック');
+}
+
+
 
 
