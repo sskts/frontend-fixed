6c06f5b3e26d4a6ee146df6594ab731f57fa8df2
akito hataguchi <hataguchi@motionpicture.jp>
2017-03-22 18:45:37 +0900

    SSKTS-11　express-form削除

--- apps/frontend/forms/Purchase/Mvtk/MvtkInputForm.js
+++ apps/frontend/forms/Purchase/Mvtk/MvtkInputForm.js
@@ -5,5 +5,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 exports.default = (req) => {
     req.checkBody('mvtk', `${req.__('common.mvtk')}${req.__('common.validation.required')}`).notEmpty();
-    req.checkBody('seats', `${req.__('common.mvtk')}${req.__('common.validation.is_json')}`).isJSON();
+    req.checkBody('mvtk', `${req.__('common.mvtk')}${req.__('common.validation.is_json')}`).isJSON();
 };
