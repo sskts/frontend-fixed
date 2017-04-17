57062f3f606a6577786768142f123138cc76e039
akito hataguchi <hataguchi@motionpicture.jp>
2017-04-17 11:47:51 +0900

    SSKTS-65　「/method/entry」削除

--- app/routes/method.js
+++ app/routes/method.js
@@ -7,7 +7,7 @@ const express = require("express");
 const MethodModule = require("../modules/Method/MethodModule");
 const router = express.Router();
 //入場方法説明
-router.get('/entry', MethodModule.entry);
+// router.get('/entry', MethodModule.entry);
 //発券方法説明
 router.get('/ticketing', MethodModule.ticketing);
 exports.default = router;
--- app/routes/method.ts
+++ app/routes/method.ts
@@ -8,7 +8,7 @@ import * as MethodModule from '../modules/Method/MethodModule';
 const router = express.Router();
 
 //入場方法説明
-router.get('/entry', MethodModule.entry);
+// router.get('/entry', MethodModule.entry);
 
 //発券方法説明
 router.get('/ticketing', MethodModule.ticketing);
