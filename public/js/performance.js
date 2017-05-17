bec2ecc123696b183ecbbecffe1d257b4ff4b2ed
akito hataguchi <hataguchi@motionpicture.jp>
2017-05-17 17:13:06 +0900

    SSKTS-331　北島施設コード追加

--- public/js/performance.js
+++ public/js/performance.js
@@ -1,5 +1,14 @@
 $(function () {
     getPerformance();
+    
+    // 照会クリック
+    $(document).on('click', '.inquiry-button a', function (event) {
+        event.preventDefault();
+        var theater = $('.inquiry-theater select').val();
+        var url = $(this).attr('href') + '?theater=' + theater;
+        location.href = url;
+    });
+    // 検索クリック
     $(document).on('click', '.search a', function (event) {
         event.preventDefault();
         getPerformance();
