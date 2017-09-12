4b719307a055dfdf734ec736081d448fa634c975
akito hataguchi <hataguchi@motionpicture.jp>
2017-09-12 18:24:08 +0900

    SSKTS-534 collection停止

--- public/js/common/collection.js
+++ public/js/common/collection.js
@@ -18,30 +18,30 @@ getCurrentPosition();
  * @returns {void}
  */
 function collection(args) {
-    var API_ENDPOINT = $('input[name=webhookApiEndPoint]').val();
-    var TIMEOUT = 5000;
-    // 必須チェック
-    if (args.client === undefined || args.label === undefined) return;
-    var data = {
-        client: args.client, // クライアントID
-        occurred_at: new Date().toISOString(), // 発生日時(ISO 8601 format)
-        label: args.label, // ラベル
-        url: location.href, // URL
-        category: (args.category === undefined) ? '' : args.category, // カテゴリー(使用方法はクライアント次第)
-        action: (args.action === undefined) ? '' : args.action, // アクション(使用方法はクライアント次第)
-        message: (args.message === undefined) ? '' : args.message, // メッセージ(使用方法はクライアント次第)
-        notes: (args.notes === undefined) ? '' : args.notes, // 備考(使用方法はクライアント次第)
-        useragent: window.navigator.userAgent, // ユーザーエージェント
-        location: collections.locations, // ユーザー位置情報
-        transaction: (args.transaction === undefined) ? '' : args.transaction, // 取引ID
-    };
-    $.ajax({
-        dataType: 'json',
-        url: API_ENDPOINT + '/clientEvents',
-        type: 'POST',
-        data: data,
-        timeout: TIMEOUT
-    });
+    // var API_ENDPOINT = $('input[name=webhookApiEndPoint]').val();
+    // var TIMEOUT = 5000;
+    // // 必須チェック
+    // if (args.client === undefined || args.label === undefined) return;
+    // var data = {
+    //     client: args.client, // クライアントID
+    //     occurred_at: new Date().toISOString(), // 発生日時(ISO 8601 format)
+    //     label: args.label, // ラベル
+    //     url: location.href, // URL
+    //     category: (args.category === undefined) ? '' : args.category, // カテゴリー(使用方法はクライアント次第)
+    //     action: (args.action === undefined) ? '' : args.action, // アクション(使用方法はクライアント次第)
+    //     message: (args.message === undefined) ? '' : args.message, // メッセージ(使用方法はクライアント次第)
+    //     notes: (args.notes === undefined) ? '' : args.notes, // 備考(使用方法はクライアント次第)
+    //     useragent: window.navigator.userAgent, // ユーザーエージェント
+    //     location: collections.locations, // ユーザー位置情報
+    //     transaction: (args.transaction === undefined) ? '' : args.transaction, // 取引ID
+    // };
+    // $.ajax({
+    //     dataType: 'json',
+    //     url: API_ENDPOINT + '/clientEvents',
+    //     type: 'POST',
+    //     data: data,
+    //     timeout: TIMEOUT
+    // });
 }
 
 /**
